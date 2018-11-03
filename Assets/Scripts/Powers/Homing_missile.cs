bea039d18c1f1ffe0eb378af55e8d8b0bb3e109d
Emilijus <emilijus@gmail.com>
2018-11-03 23:42:18 +0200

    Added first version of homing missiles (they should follow now)

---
+++ Assets/Scripts/Powers/Homing_missile.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homing_missile : MonoBehaviour {
+
+	private List<Transform> _characters = new List<Transform>();
+
+	public float distanceToFollow;
+	public float angleChangingSpeed;
+	public float movementSpeed;
+	public GameObject owner;
+
+	// Use this for initialization
+	void Start () {
+		// Get all other characters to follow
+		var characters = GameObject.FindGameObjectsWithTag("Character");
+		
+		foreach(GameObject c in characters)
+		{
+			var character = c.transform.parent.parent;
+
+			if(character.gameObject != owner)
+			{
+				_characters.Add(character);
+			}
+		}
+	}
+	
+	void FixedUpdate ()
+    {
+		Transform target = null;
+
+		foreach(Transform c in _characters)
+		{
+			if(!target || 
+			Vector3.Distance(transform.position, c.position) <
+			Vector3.Distance(transform.position, target.position))
+			{
+				target = c;
+			}
+		}
+
+		if(!target || distanceToFollow < Vector3.Distance(transform.position, target.position))
+		{
+			flyStraight();
+		}
+		else
+		{
+			followTarget(target);
+		}
+    }
+
+	private void flyStraight() {
+		var rb = GetComponent<Rigidbody>();
+
+		rb.velocity = transform.forward * movementSpeed;
+	}
+
+	private void followTarget(Transform target) {
+		var direction = (target.position - transform.position).normalized;
+		var lookRotation = Quaternion.LookRotation(direction);
+
+		transform.rotation = Quaternion.Slerp (transform.rotation, lookRotation, Time.deltaTime * angleChangingSpeed);
+		
+		flyStraight();
+	}
+}
