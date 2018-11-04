8688704c02dd75cf572b644dc00f5c8ee9ab6a90
Karolis <karolis.uzkuraitis@gmail.com>
2018-11-04 21:23:36 +0200

    Added initial view

---
+++ Assets/Scripts/UI/ShowPlayerHPScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPlayerHPScreen : MonoBehaviour
+{
+	private GameObject[] _allPlayers;
+
+	void Start ()
+	{
+		_allPlayers = GameObject.FindGameObjectsWithTag("Player");
+		foreach (var player in allPlayers)
+		{
+			var inputManager = player.GetComponent<PlayerInputManager>();
+			transform.gameObject.transform.GetChild(0).GetComponent<Text>().text = "Player" + inputManager.PlayerId;
+			transform.gameObject.transform.GetChild(1).GetComponent<Text>().text = player.GetComponent<PlayerController>().HP;
+		}
+	}
+}
