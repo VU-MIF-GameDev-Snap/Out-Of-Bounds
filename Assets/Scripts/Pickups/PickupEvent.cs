ebc28b3c36688ea09ad81e3ef8c582dc88f0cc93
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2018-11-04 22:04:11 +0200

    Make CollisionBox for character permanent

--- Assets/Scripts/Pickups/PickupEvent.cs
+++ Assets/Scripts/Pickups/PickupEvent.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class PickupEvent : MonoBehaviour
 {
-    public GameObject ParentBody;
+    public GameObject PlayerModel;
 
     private void OnCollisionEnter(Collision collision)
     {
         // TODO: Won't work if weapon is on the ground
         if (collision.gameObject.CompareTag("Weapon"))
         {
-            ParentBody.SendMessage("OnWeaponPickup", collision.gameObject);
+            PlayerModel.SendMessage("OnWeaponPickup", collision.gameObject);
         }
     }
 }
