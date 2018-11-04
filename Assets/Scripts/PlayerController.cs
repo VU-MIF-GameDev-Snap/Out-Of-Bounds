03a756a1c4c4db486b9b1727d73d63bd1526f5dc
Karolis <karolis.uzkuraitis@gmail.com>
2018-11-04 09:34:28 +0200

    Add multiple player controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInputManager))]
 public class PlayerController : MonoBehaviour {
 
     [Header("Player controller variables")]
@@ -15,26 +16,29 @@ public class PlayerController : MonoBehaviour {
     private CharacterController _controller;
     private Vector3 _velocity;
     private Animator _animator;
+    private PlayerInputManager _inputManager;
 
     void Start ()
     {
         _controller = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
+        _inputManager = GetComponent<PlayerInputManager>();
     }
 
     void Update ()
     {
-        if (Input.GetButton("Jump") && _controller.isGrounded)
+        if (_inputManager.IsButtonDown(PlayerInputManager.Key.Jump) && _controller.isGrounded)
         {
             _velocity.y += Mathf.Sqrt(JumpHeight * -2f * Gravity);
             Debug.Log("jump");
             _animator.SetTrigger("Jump");
         }   
+        var horizontalInput = _inputManager.GetAxis(PlayerInputManager.Key.MoveHorizontal);
+        Vector3 move = new Vector3(horizontalInput, 0, 0);
+        _animator.SetFloat ("Speed", Mathf.Abs(horizontalInput));
+        _animator.SetBool ("IsGrounded", _controller.isGrounded);
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-        _animator.SetFloat ("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
-
-        if (Input.GetButtonDown("Dash"))
+        if (_inputManager.IsButtonDown(PlayerInputManager.Key.Dash))
         {
             Debug.Log("Dash");
             var dashingVelocity = Vector3.Scale(move, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * Drag.x + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * Drag.z + 1)) / -Time.deltaTime)));
@@ -54,7 +58,7 @@ public class PlayerController : MonoBehaviour {
 
         _velocity.x /= 1 + Drag.x * Time.deltaTime;
         _velocity.y /= 1 + Drag.y * Time.deltaTime;
-        Debug.Log("velo: " + _velocity + " + grounded: " + _controller.isGrounded);
+        // Debug.Log("velo: " + _velocity + " + grounded: " + _controller.isGrounded);
         _controller.Move((_velocity + (move * Speed)) * Time.deltaTime);
         
         if (move != Vector3.zero)
---
+++ Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+	[Header("Who controls this player")]
+	public int PlayerId = 1;
+
+	public enum Key
+	{
+		Jump,
+		Dash,
+		MoveHorizontal,
+		MoveVertical,
+	}
+
+
+	private Dictionary<Key, string> _playerKeyMappings = new Dictionary<Key, string>();
+
+	public void Start ()
+	{
+		var values = Enum.GetValues(typeof(Key)).Cast<Key>();
+		foreach (var item in values)
+		{
+			_playerKeyMappings.Add(item, "Player" + PlayerId + "_" + item.ToString("G"));
+		}
+	}
+
+	public bool IsButtonDown (Key key)
+	{
+		string value;
+		if(!_playerKeyMappings.TryGetValue(key, out value))
+		{
+			return false;
+		}
+		
+		return Input.GetButton(value);
+	}
+
+	public bool IsButtonPressed (Key key)
+	{
+		string value;
+		if(!_playerKeyMappings.TryGetValue(key, out value))
+		{
+			return false;
+		}
+		
+		return Input.GetButtonDown(value);
+	}
+
+	public float GetAxis (Key key)
+	{
+		string value;
+		if(!_playerKeyMappings.TryGetValue(key, out value))
+		{
+			return 0;
+		}
+		
+		return Input.GetAxis(value);
+	}
+}
