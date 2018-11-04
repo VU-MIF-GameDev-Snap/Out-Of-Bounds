a716c1a6d6edc35c7d346697d893fdac5f49b8d3
Gediminas Krasauskas <gedgeds@gmail.com>
2018-11-04 13:16:38 +0200

    Bullets destroyed on contact with player. Weapons and bullets destroyed on contact with boundary

---
+++ Assets/Scripts/Weapons/DestroyByContactBolt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DestroyByContactBolt : MonoBehaviour {
+    
+	private GameController _gameController;
+
+	void Start() {
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
