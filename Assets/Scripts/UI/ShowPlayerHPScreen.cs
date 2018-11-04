b0723d4bdbd60547597a546ed1ba8a3da18aa8d1
Karolis <karolis.uzkuraitis@gmail.com>
2018-11-04 22:17:35 +0200

    + font size

--- Assets/Scripts/UI/ShowPlayerHPScreen.cs
+++ Assets/Scripts/UI/ShowPlayerHPScreen.cs
@@ -6,16 +6,42 @@ using UnityEngine.UI;
 
 public class ShowPlayerHPScreen : MonoBehaviour
 {
+	private class HealthTracker
+	{
+		public PlayerInputManager PlayerIM;
+		public PlayerController PlayerC;
+		public GameObject Text;
+	}
+
 	private GameObject[] _allPlayers;
+	private IList<HealthTracker> _healthTrackers = new List<HealthTracker>();
+
+	public GameObject PlayerHpShowPrefab;
 
 	void Start ()
 	{
 		_allPlayers = GameObject.FindGameObjectsWithTag("Player");
-		foreach (var player in allPlayers)
+
+		foreach (var player in _allPlayers)
 		{
 			var inputManager = player.GetComponent<PlayerInputManager>();
-			transform.gameObject.transform.GetChild(0).GetComponent<Text>().text = "Player" + inputManager.PlayerId;
-			transform.gameObject.transform.GetChild(1).GetComponent<Text>().text = player.GetComponent<PlayerController>().HP;
+
+			var hpShow = Instantiate(PlayerHpShowPrefab, new Vector3(), new Quaternion());
+			hpShow.transform.SetParent(transform, false);
+			_healthTrackers.Add(new HealthTracker{
+				PlayerIM = player.GetComponent<PlayerInputManager>(),
+				PlayerC = player.GetComponent<PlayerController>(),
+				Text = hpShow
+			});
+		}
+	}
+
+	void Update ()
+	{
+		foreach (var healthTracker in _healthTrackers)
+		{
+			healthTracker.Text.transform.GetChild(0).GetComponent<Text>().text = "Player" + healthTracker.PlayerIM.PlayerId;
+			healthTracker.Text.transform.GetChild(1).GetComponent<Text>().text = "hp: " + healthTracker.PlayerC.hitpoints;
 		}
 	}
 }
