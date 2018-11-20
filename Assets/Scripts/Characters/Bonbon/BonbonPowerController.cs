7f5dd1ba4a9fdd3828b26f291baf5185edc4ea98
Karolis <karolis.uzkuraitis@gmail.com>
2018-11-20 08:05:30 +0200

    Bonbon ChargePunch limited max damage & knockback

--- Assets/Scripts/Characters/Bonbon/BonbonPowerController.cs
+++ Assets/Scripts/Characters/Bonbon/BonbonPowerController.cs
@@ -48,6 +48,8 @@ public class BonbonPowerController : MonoBehaviour, ICharacterPowerController
     float ChargePuchDamagePerSecond = 10f;
     [SerializeField]
     float ChargePuchKnockbackPerSecond = 10f;
+    [SerializeField]
+    float ChargePunchMaxChargeDuration = 15f;
 
     [Header("StoneForm")]
     [SerializeField]
@@ -64,6 +66,8 @@ public class BonbonPowerController : MonoBehaviour, ICharacterPowerController
     float StoneFormMaxDuration = 6f;
     [SerializeField]
     float StoneFormMinDuration = 1.2f;
+    [SerializeField]
+    float StoneFormActivationDelay = 1f;
     [SerializeField]
     Material OriginalMaterial;
     [SerializeField]
@@ -99,33 +103,45 @@ public class BonbonPowerController : MonoBehaviour, ICharacterPowerController
         {
             _chargePunchActive = false;
             _chargePunchCooldownStartTime = Time.time;
-
-            // * Consider reseting abilities a little later, after animation is done.
-            foreach (var ability in _chargePunchAbilitiesToTurnOff)
-            {
-                _playerController.AbilityToggle(ability, true);
-            }
         }
     }
 
     public void OnChargePunchLoop ()
     {
+        var currentDuration = Time.time - _chargePunchStartTime;
+        if(currentDuration > ChargePunchMaxChargeDuration)
+        {
+            currentDuration = ChargePunchMaxChargeDuration;
+        }
+
         var sphericalShape = ChargePunchChargingParticleSystem.shape;
-        var newRadius = Mathf.Clamp((Time.time - _chargePunchStartTime) / 10f, 0.1f, 1.5f);
+        var minRadius = 0.1f;
+        var maxRadius = 1.5f;
+        var newRadius = Mathf.Clamp((currentDuration / ChargePunchMaxChargeDuration) * (maxRadius - minRadius) + minRadius, minRadius, maxRadius);
+        sphericalShape.radius = newRadius;
+
         var particleMainOptions = ChargePunchChargingParticleSystem.main;
         particleMainOptions.startSpeed = -newRadius;
-        particleMainOptions.startLifetime = newRadius;
-        sphericalShape.radius = newRadius;
 
         ChargePunchChargingParticleSystem.Play();
     }
 
     public void OnChargePunchFinish ()
     {
+        var currentDuration = Time.time - _chargePunchStartTime;
+        if(currentDuration > ChargePunchMaxChargeDuration)
+        {
+            currentDuration = ChargePunchMaxChargeDuration;
+        }
+
         var coneShape = ChargePunchFinishParticleSystem.shape;
-        coneShape.angle = Mathf.Clamp((Time.time - _chargePunchStartTime) * 4.6f, 10f, 70f);
+        var minAngle = 10f;
+        var maxAngle = 70f;
+        coneShape.angle = Mathf.Clamp((currentDuration / ChargePunchMaxChargeDuration) * (maxAngle - minAngle) + minAngle, minAngle, maxAngle);
         var particleEmissionOptions = ChargePunchFinishParticleSystem.emission;
-        particleEmissionOptions.rateOverTime = Mathf.Clamp((Time.time - _chargePunchStartTime) * 26f, 50f, 400f);
+        var minEmissionRate = 50f;
+        var maxEmissionRate = 400f;
+        particleEmissionOptions.rateOverTime = Mathf.Clamp((currentDuration / ChargePunchMaxChargeDuration) * (maxEmissionRate - minEmissionRate) + minEmissionRate, minEmissionRate, maxEmissionRate);
         ChargePunchFinishParticleSystem.Play();
 
         var playerCollisions = Physics.OverlapBox(ChargePunchFinishParticleSystem.transform.position + ChargePunchFinishParticleSystem.transform.forward * ChargePunchFinishParticleSystem.shape.scale.z/2, ChargePunchFinishParticleSystem.shape.scale, ChargePunchFinishParticleSystem.transform.rotation)
@@ -139,13 +155,19 @@ public class BonbonPowerController : MonoBehaviour, ICharacterPowerController
         {
             var hitMessage = new HitMessage
             {
-                Damage = (int)((Time.time - _chargePunchStartTime) * ChargePuchDamagePerSecond),
+                Damage = (int)(currentDuration * ChargePuchDamagePerSecond),
                 HitType = HitType.None,
                 KnockbackDirection = ChargePunchFinishParticleSystem.transform.forward,
-                KnockbackValue = (int)((Time.time - _chargePunchStartTime) * ChargePuchKnockbackPerSecond),
+                KnockbackValue = (int)(currentDuration * ChargePuchKnockbackPerSecond),
             };
 
             player.OnHit(hitMessage);
+        }
+
+        // After animation is done reset abilities.
+        foreach (var ability in _chargePunchAbilitiesToTurnOff)
+        {
+            _playerController.AbilityToggle(ability, true);
         }
     }
 
@@ -157,14 +179,17 @@ public class BonbonPowerController : MonoBehaviour, ICharacterPowerController
             return;
         }
         _stoneFormActive = true;
+        var formingParticlesMain = StoneFormFormingParticleSystem.main;
+        formingParticlesMain.duration = StoneFormActivationDelay;
         StoneFormFormingParticleSystem.Play();
+
         _stoneFormAlreadyHealedAmount = 0;
         _stoneFormStartTime = Time.time;
     }
 
     public void HandleStoneForm ()
     {
-        if(!_stoneFormActive || (_stoneFormStartTime + 1 > Time.time))
+        if(!_stoneFormActive || (_stoneFormStartTime + StoneFormActivationDelay > Time.time))
         {
             return;
         }
@@ -176,12 +201,13 @@ public class BonbonPowerController : MonoBehaviour, ICharacterPowerController
             {
                 _playerController.AbilityToggle(ability, false);
             }
+            // Make player invincible
             _playerController.DamageResistance = 1f;
             _playerController.KnockbackResistance = 1f;
             _animator.StartPlayback();
         }
 
-        var duration = Time.time - _stoneFormStartTime - 1;
+        var duration = Time.time - _stoneFormStartTime - StoneFormActivationDelay;
         int rightNowHealAmount = (int)Math.Ceiling(duration * StoneFormHealingPerSecond);
         if(rightNowHealAmount - _stoneFormAlreadyHealedAmount > 0)
         {
@@ -198,6 +224,7 @@ public class BonbonPowerController : MonoBehaviour, ICharacterPowerController
             _stoneFormActive = false;
             _stoneFormCooldownStartTime = Time.time;
             StoneFormBreakParticleSystem.Play();
+            // Player can get hurt again
             _playerController.DamageResistance = 0f;
             _playerController.KnockbackResistance = 0f;
 
