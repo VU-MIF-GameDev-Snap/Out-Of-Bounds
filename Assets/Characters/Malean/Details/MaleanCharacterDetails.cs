8b5695f58fdee53fbec2f7aef7812a37f6a8f56f
Emilijus <emilijus@gmail.com>
2018-12-01 11:58:28 +0200

    Malean enters the arena

---
+++ Assets/Characters/Malean/Details/MaleanCharacterDetails.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaleanCharacterDetails : ICharacterDetails
+{
+
+    public string PreviewResource
+    {
+        get
+        {
+            return "Characters/Malean/Preview";
+        }
+    }
+
+	string ICharacterDetails.Name
+    {
+        get
+        {
+            return "Malean";
+        }
+    }
+
+    public string Power1Description
+    {
+        get
+        {
+            return "Cyborg Strike - strikes very hard up or down";
+        }
+    }
+
+    public string Power2Description
+    {
+        get
+        {
+            return "Black hole - casts a black hole which sucks";
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return "Malean was specifically created to fight in arenas. It doesn't have feelings nor emotions, it's only purpose is to win. Every time Malean loses its memory is wiped to cleanly from this sad occurence.";
+        }
+    }
+
+    public GameObject CharacterPrefab
+    {
+        get
+        {
+            return Resources.Load("Characters/Malean/Malean") as GameObject;
+        }
+    }
+}
