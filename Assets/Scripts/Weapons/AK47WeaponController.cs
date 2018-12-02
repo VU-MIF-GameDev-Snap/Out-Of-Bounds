1c2cd28c4c416331768d4cf5d14501bee66a1778
Emilijus <emilijus@gmail.com>
2018-12-02 16:18:03 +0200

    Fix baseball bats not disappearing

--- Assets/Scripts/Weapons/AK47WeaponController.cs
+++ Assets/Scripts/Weapons/AK47WeaponController.cs
@@ -7,10 +7,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class AK47WeaponController : BaseWeaponController
 {
-	[Tooltip("Seconds after which the gun will disappear")]
-	public int SelfDestructTimer = 5;
-	private float _timeToDie = 0;
-
 	[Header("Weapon attributes")]
 	public float FireRate = 0.1f;
 	public int Ammo = 5;
@@ -22,23 +18,7 @@ public class AK47WeaponController : BaseWeaponController
 	public Transform shotSpawn;
 	public Transform LeftHandPosition;
 
-
-	private AudioSource _audio;
 	private float _nextFire;
-
-	private Transform _originalParent;
-
-	void Start ()
-	{
-		_audio = GetComponent<AudioSource>();
-		_originalParent = gameObject.transform.root;
-	}
-
-	void Update ()
-	{
-		if (ShouldSelfDestruct() && !IsBeingHeld())
-			Destroy(gameObject);
-	}
 
 	public override void Fire ()
 	{
@@ -60,20 +40,6 @@ public class AK47WeaponController : BaseWeaponController
 	public Transform GetLeftHandPosition ()
 	{
 		return LeftHandPosition;
-	}
-
-	private bool ShouldSelfDestruct ()
-	{
-		// Death timer not set yet
-		if (_timeToDie == 0)
-			_timeToDie = Time.time + SelfDestructTimer;
-
-		return _timeToDie <= Time.time;
-	}
-
-	private bool IsBeingHeld ()
-	{
-		return _originalParent != gameObject.transform.root;
 	}
 
 	public override WeaponType GetWeaponType ()
